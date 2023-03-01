c44997c2f6bb661ce1bf96594d697386f1d3eca5
qoinpal <125351828+qoinpal@users.noreply.github.com>
2023-03-01 11:04:58 +0100

    Update WaitlistRegistration.jsx

--- src/pages/WaitlistRegistration/WaitlistRegistration.jsx
+++ src/pages/WaitlistRegistration/WaitlistRegistration.jsx
@@ -9,7 +9,7 @@ import logoMobile from "./arrow-left.png";
 
 export default function JoinWaitlist() {
   const { width } = useWindowSize();
-  const mobile = max-width: 480;
+  const mobile = width <= 480;
 
   return (
     <>
