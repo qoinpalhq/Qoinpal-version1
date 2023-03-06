ff414ab29a9c5f2bc93b07759bf68257c892fbc7
Oyebola Ebunoluwa <oyebolaebun07@gmail.com>
2023-03-06 23:32:38 +0100

    loder

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import "./loader.css";
+import loader from "./loader.module.css";
+import logo from "/src/assets/logo/qoinpal2-black.svg";
 
 function Loader() {
   return (
-    <div className="loader-container">
-      <div className="loader-logo"></div>
+    <div className={loader.container}>
+      <div className={loader.logo}>
+        <img src={logo} />
+      </div>
     </div>
   );
 }
