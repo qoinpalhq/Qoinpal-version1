ce0c3b5d64d6c7630f2c4e20782e941dd234f36e
qoinpal <125351828+qoinpal@users.noreply.github.com>
2023-03-07 09:26:14 +0100

    Desktop only animation cta

--- src/components/Cta/Cta.jsx
+++ src/components/Cta/Cta.jsx
@@ -3,16 +3,37 @@ import arrow from "../../assets/arrow.png";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import useLoad from "../../hook/useLoad";
+import useWindowSize from " /src/hook/useWindowSize";
 
 export default function Cta() {
   const { isLoaded } = useLoad();
+  const {width} = useWindowSize();
+  const desktop = width > 767;
   return (
     <section className={cta.cta}>
       <div className={cta.decor}></div>
-      <div
+<motion.div
+
         className={`${cta.content}  global-content-wrapper`}
-       
-       
+
+        initial={desktop ? {opacity: 0, y: `100%` }: {}}
+
+        animate={(desktop && isLoaded) ? { opacity: 1, y: `0` } : {}}
+
+        transtion={{
+
+          type: "tween",
+
+          damping: 0.8,
+
+          duration: 4,
+
+          delay: 10,
+
+          staggerChildren: 3,
+
+        }}
+
       >
         <Link to="registration/waitlist" className={` ${cta.a}`}>
           <h2>
